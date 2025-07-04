d439204d945cdf0053d249db03e1697141ac0c0e
Sedanur Yeşilnacar <61359680+SedanurYesilnacar@users.noreply.github.com>
2025-07-04 16:48:45 +0300

    Camera bounds added

--- Assets/_GameData/Scripts/CameraController.cs
+++ Assets/_GameData/Scripts/CameraController.cs
@@ -5,8 +5,14 @@ namespace _GameData.Scripts
     public class CameraController : MonoBehaviour
     {
         [SerializeField] private Vector3 offset;
+        [SerializeField] private float leftBound;
+        [SerializeField] private float rightBound;
+        [SerializeField] private float topBound;
+        [SerializeField] private float bottomBound;
 
         private Transform _target;
+
+        private Vector3 _desiredPosition;
 
         private void OnEnable()
         {
@@ -26,8 +32,13 @@ namespace _GameData.Scripts
         private void LateUpdate()
         {
             if(_target == null) return;
-            
-            transform.position = _target.position + offset;
+
+            _desiredPosition = _target.position + offset;
+            float finalXPosition = Mathf.Clamp(_desiredPosition.x, leftBound, rightBound);
+            float finalZPosition = Mathf.Clamp(_desiredPosition.z, bottomBound, topBound);
+
+            transform.position = new Vector3(finalXPosition, _desiredPosition.y, finalZPosition);
+
         }
     }
 }
